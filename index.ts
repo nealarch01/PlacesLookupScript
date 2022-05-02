6acd4045596c6c09caf949a70095c7a1ec1d43e7
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-05-01 17:06:47 -0700

    Generate more places

--- index.ts
+++ index.ts
@@ -3,16 +3,10 @@ import axios from "axios";
 
 import { google_api_key } from "./config.json";
 
+let GLOBAL_SUM = 0;
+
 function formatQueryString(queryText: string): string {
-    if (queryText.includes(' ')) {
-        queryText = queryText.replaceAll(' ', '%20');
-    }
-    if (queryText.includes("'")) {
-        queryText = queryText.replaceAll("'", "%27");
-    }
-    if (queryText.includes(",")) {
-        queryText = queryText.replaceAll(",", "%2C");
-    }
+    queryText = encodeURI(queryText);
     return queryText;
 }
 
@@ -23,16 +17,23 @@ function formatFilenameString(queryText: string): string {
     if (queryText.includes("'")) {
         queryText = queryText.replaceAll("'", "");
     }
+    queryText = queryText.replaceAll(" ", "-");
+    queryText = queryText.replaceAll("'", "");
+    queryText = queryText.replaceAll(",", "");
     return queryText;
 }
 
 
-function writeData(placeData: any, placeType: string): void {
-    fs.writeFileSync(`./PlacesData/${formatFilenameString(placeType)}.json`, JSON.stringify(placeData, null, 4));
+function writeData(placeData: any, placeType: string, location: string): void {
+    try {
+        fs.writeFileSync(`./MorePlacesData/${formatFilenameString(`${placeType}-${location}`)}.json`, JSON.stringify(placeData, null, 4));
+    } catch (err) {
+        console.log("There was an error writing");
+    }
 }
 
-async function fetchData(placeType: string): Promise<any> {
-    placeType += "near san marcos, ca";
+async function fetchPlaceData(placeType: string, location: string): Promise<any> {
+    placeType += ` in ${location}`;
     const url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${formatQueryString(placeType)}&key=${google_api_key}`
     try {
         let response = await axios.get(url);
@@ -42,24 +43,40 @@ async function fetchData(placeType: string): Promise<any> {
             return undefined;
         }
     } catch (err) {
-        console.log("There was an error");
+        console.log("There was an error making an API request");
         return undefined;
     }
 }
 
-async function main(): Promise<number> {
-   const placesOfInterest: Array<string> = [
-       "cafe", "amusement park", "aquarium", "bar", "night club", "thrift stores",
-       "shopping malls", "farmers' market", "casino", "bakery", "restaurant",
-       "bowling", "arcade", "parks", "trails"
-   ];
+async function processPlacesOfInterest(placesOfInterest: Array<string>, location: string): Promise<void> {
     placesOfInterest.forEach(async (placeType: string) => {
-        let placeData = await fetchData(placeType);
+        let placeData = await fetchPlaceData(placeType, location);
         if (placeData !== undefined) {
-            writeData(placeData, placeType); // write to file
+            writeData(placeData, placeType, location); // Write to file
         }
     });
+}
+
+async function main(): Promise<number> {
+    const placesOfInterest_NorthCountySD: Array<string> = [
+        "farmers' markets", "beaches", "cafes", "shopping malls",
+        "amusement parks", "arcades", "parks", "bowling", "bakeries", "museums",
+        "movie theatres", "libraries", "tea shops", "trails", "skate parks", "night clubs",
+        "thrift stores", "bars", "breweries", "brunch", "escape rooms", "vegan food"
+    ];
+
+    const placesOfInterest_SanDiego: Array<string> = [
+        "art gallery", "art museums", "beaches", "date night", "thrift stores", "museums", "casino", "bakeries",
+        "cafes", "fine dining", "night club", "aquarium", "amusement parks", "shopping malls", "brunch", "wineries"
+    ];
+
+    await processPlacesOfInterest(placesOfInterest_NorthCountySD, "north county san diego, ca");
+    await processPlacesOfInterest(placesOfInterest_SanDiego, "san diego, ca");
+    console.log(placesOfInterest_NorthCountySD.sort());
+    console.log(placesOfInterest_SanDiego.sort());
+    console.log("Successful run");
     return 0;
 }
+
 
 main();
